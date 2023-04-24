896b83ce12872e007929c5c0741e70cfc6929617
Nurlan Pirjanov <pirjanovnurlan77@gmail.com>
2023-04-25 00:53:48 +0500

    add environs

--- todo/settings.py
+++ todo/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
-import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(c=ouws!og__g#&nbe8h1iyw4-=w9h3&+x^qh$9p+e)o)l@sb@'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
